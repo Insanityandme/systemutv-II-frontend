6f87841fb7bcdfa7bf89873e26166c61bc7489b5
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2024-02-05 14:00:55 +0100

    h1 color fixed

--- systemutv-2-frontend/src/Pages/Dashboard/Dashboard.js
+++ systemutv-2-frontend/src/Pages/Dashboard/Dashboard.js
@@ -2,10 +2,10 @@ import './Dashboard.css';
 import Navbar from "../../Navbar";
 const Dashboard = () => {
     return (
-        <>
+        <div className={"dashboard"}>
             <Navbar/>
             <h1>Dashboard page</h1>
-        </>
+        </div>
     );
 }
 export default Dashboard;
