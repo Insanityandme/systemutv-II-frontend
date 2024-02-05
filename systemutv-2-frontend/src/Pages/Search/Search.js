b649c18268404ac295926d1173819d345710c99a
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2024-02-05 16:47:36 +0100

    Search page is created
    Without flower element

--- systemutv-2-frontend/src/Pages/Search/Search.js
+++ systemutv-2-frontend/src/Pages/Search/Search.js
@@ -4,23 +4,28 @@ import {useState} from "react";
 
 const Search = () => {
 
-    const [filter, setFilter] = useState('No filter');
+    const [filter, setFilter] = useState('');
 
     const handleSelectChange = (e) => {
-        setFilter()
+        setFilter(e.target.value);
     }
 
     return (
         <div className={"search"}>
             <Navbar/>
             <div className={"main-panel-search"}>
-                <input/>
-                <button>Search</button>
-                <select id="dropdown" value={filter} onChange={handleSelectChange}>
-                    <option value="default">All plants</option>
-                    <option value="Watered">Watered</option>
-                    <option value="NotWatered">Not watered</option>
-                </select>
+                <div className={"search-buttons"}>
+                    <input placeholder={"Search text here"}/>
+                    <button>Search</button>
+                    <select id="dropdown" value={filter} onChange={handleSelectChange}>
+                        <option value="common">Common name</option>
+                        <option value="scientific">Scientific name</option>
+                    </select>
+                </div>
+
+                <div className={"search-result-panel"}>
+                    <h2>I am flower</h2>
+                </div>
 
             </div>
         </div>
