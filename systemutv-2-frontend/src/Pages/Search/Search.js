5316d87a9143f1a5b2be6fe62adc27cf551b2018
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2024-02-05 15:59:37 +0100

    creating search page

--- systemutv-2-frontend/src/Pages/Search/Search.js
+++ systemutv-2-frontend/src/Pages/Search/Search.js
@@ -1,12 +1,29 @@
 import './Search.css';
 import Navbar from "../../Navbar";
+import {useState} from "react";
 
 const Search = () => {
+
+    const [filter, setFilter] = useState('No filter');
+
+    const handleSelectChange = (e) => {
+        setFilter()
+    }
+
     return (
-        <>
+        <div className={"search"}>
             <Navbar/>
-            <h1>Search page</h1>
-        </>
+            <div className={"main-panel-search"}>
+                <input/>
+                <button>Search</button>
+                <select id="dropdown" value={filter} onChange={handleSelectChange}>
+                    <option value="default">All plants</option>
+                    <option value="Watered">Watered</option>
+                    <option value="NotWatered">Not watered</option>
+                </select>
+
+            </div>
+        </div>
     );
 }
 export default Search;
