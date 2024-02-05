8531efa114923fd76ed8552d926192a6aa04fd81
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2024-02-05 12:20:16 +0100

    bare bones Login and Register page
    (without any database call)

--- systemutv-2-frontend/src/Pages/Login/Login.js
+++ systemutv-2-frontend/src/Pages/Login/Login.js
@@ -1,14 +1,22 @@
 import './Login.css';
-import { useNavigate } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 const Login = () => {
     const navigate = useNavigate();
     return (
-        <>
-            <h1>Login page</h1>
-            <button onClick={()=> navigate('/register')}>Create account</button>
-            <button onClick={() => navigate('/')}>Log in</button>
-        </>
+        <div className={"login"}>
+            <div className="login-container">
+                <h1>Welcome to your garden</h1>
+                <div className="form-container">
+                    <form action="#" method="post" className={"inputs-container"}>
+                        <input type="email" id="email" name="email" placeholder="Enter your email" required/>
+                        <input type="password" id="password" name="password" placeholder="Enter your password" required/>
+                        <button onClick={() => navigate('/')}>Log in</button>
+                    </form>
+                </div>
+                <h5>Don't have an account? <Link to={"/register"}>Create account</Link> </h5>
+            </div>
+        </div>
     );
 }
 export default Login;
--- systemutv-2-frontend/src/Pages/Login/Register.js
+++ systemutv-2-frontend/src/Pages/Login/Register.js
@@ -1,14 +1,50 @@
 import './Login.css';
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 const Register = () => {
     const navigate = useNavigate();
+
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [passwordMatch, setPasswordMatch] = useState(false);
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleConfirmPasswordChange = (e) => {
+        setConfirmPassword(e.target.value);
+        checkPasswordMatch(password, e.target.value);
+    };
+
+    const checkPasswordMatch = (password, confirmPassword) => {
+        if (password === confirmPassword) {
+            setPasswordMatch(true);
+        } else {
+            setPasswordMatch(false);
+        }
+    };
+
+    const Submit = () => {
+       navigate("/");
+    }
+
     return (
-        <>
-            <h1>Register page</h1>
-            <button onClick={()=>navigate('/login')} >I have an account</button>
-            <button onClick={() => navigate('/')} >Register</button>
-        </>
+        <div className={"login"}>
+            <div className="login-container">
+                <h1>Create your garden</h1>
+                <div className="form-container">
+                    <form action="#" method="post" className={"inputs-container"}>
+                        <input type="email" id="email" name="email" placeholder="Enter your email" required/>
+                        <input type="password" id="password" name="password" placeholder="Enter your password" onChange={handlePasswordChange} required/>
+                        <input type="password" id="confirm" name="password" placeholder="Confirm your password" onChange={handleConfirmPasswordChange} style={{ borderColor: passwordMatch ? "#386641" : "#bc4749" }} required/>
+                        <button onClick={Submit} disabled={!passwordMatch}>Register</button>
+                    </form>
+                </div>
+                <h5>Already have an account? <Link to={"/login"}>Login here</Link> </h5>
+            </div>
+        </div>
     );
 }
 export default Register;
