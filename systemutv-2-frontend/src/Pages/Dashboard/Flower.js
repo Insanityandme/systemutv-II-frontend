aeedeecc553961f81c7380f96f2d8f238248c72a
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2024-02-09 11:48:33 +0100

    Redirection is made with additional fixes
    Flower element is now ready for database, it is completely done.
    Placing flowers is now at final form ready for database fetch.

--- systemutv-2-frontend/src/Pages/Dashboard/Flower.js
+++ systemutv-2-frontend/src/Pages/Dashboard/Flower.js
@@ -1,6 +1,6 @@
 import {useState} from "react";
 
-export const Flower = (image, commonName, scientificName, info) => {
+export const Flower = (props) => {
     const [showInfo, setShowInfo] = useState(false);
 
     const handleInfo = () => {
@@ -9,28 +9,17 @@ export const Flower = (image, commonName, scientificName, info) => {
 
     return (
         <div className={"flower"}>
-            {/*
-            <img alt={"flower"} className={"flower-image"} src={image}/>
-            <p>{commonName}</p>
-            <p>{scientificName}</p>
-            <p>{info}</p>
-            */}
             <div className={"flower-left-side"}>
-                <div className={"flower-image"}></div>
-                <p>commonName</p>
+                <img alt={"flower"} className={"flower-image"} src={props.image}/>
+                <p>{props.commonName}</p>
             </div>
-            <p>scientificName</p>
+            <p>{props.scientificName}</p>
             <div className={"flower-info"}>
                 <div className={"flower-info-expand"}>
                     <button onClick={handleInfo}>info</button>
                     {showInfo ?
                         <p className={"info"}>
-                            Lorem ipsum dolor sit amet,
-                            consectetur adipiscing elit.
-                            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat.
-                            Duis aute irure dolor in.
+                            {props.info}
                         </p>
                         :
                         <div></div>
