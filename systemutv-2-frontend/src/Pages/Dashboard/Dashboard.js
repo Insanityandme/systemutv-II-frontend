dd932d1789f815a1eebac344018f9c805dadda16
Filip Bengtegård <bengtegardbook@gmail.com>
2024-02-16 23:05:17 +0100

    able to search plants

--- systemutv-2-frontend/src/Pages/Dashboard/Dashboard.js
+++ systemutv-2-frontend/src/Pages/Dashboard/Dashboard.js
@@ -5,116 +5,116 @@ import Flower from "./Flower";
 import {useNavigate} from "react-router-dom";
 
 const Dashboard = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [selectedOption, setSelectedOption] = useState('option1');
-    const [flowers, setFlowers] = useState([]);
+  const [selectedOption, setSelectedOption] = useState('option1');
+  const [flowers, setFlowers] = useState([]);
 
-    //TODO: OBS! DELETE THIS PUSH METHOD AFTER JOINING WITH DATABASE !!!ONLY FOR DISPLAY PURPOSES!!!
-    flowers.push({
-        image: "",
-        commonName: "Flower",
-        scientificName: "Flos",
-        info: "A flower, also known as a bloom or blossom, " +
-            "is the reproductive structure found in flowering plants " +
-            "(plants of the division Angiospermae). " +
-            "Flowers consist of a combination of vegetative organs – " +
-            "sepals that enclose and protect the developing flower, petals " +
-            "that attract pollinators, and reproductive organs that produce gametophytes, " +
-            "which in flowering plants produce gametes. The male gametophytes, which produce sperm, " +
-            "are enclosed within pollen grains produced in the anthers. The female gametophytes are " +
-            "contained within the ovules produced in the carpels."
-    });
+  //TODO: OBS! DELETE THIS PUSH METHOD AFTER JOINING WITH DATABASE !!!ONLY FOR DISPLAY PURPOSES!!!
+  flowers.push({
+    image: "",
+    commonName: "Flower",
+    scientificName: "Flos",
+    info: "A flower, also known as a bloom or blossom, " +
+      "is the reproductive structure found in flowering plants " +
+      "(plants of the division Angiospermae). " +
+      "Flowers consist of a combination of vegetative organs – " +
+      "sepals that enclose and protect the developing flower, petals " +
+      "that attract pollinators, and reproductive organs that produce gametophytes, " +
+      "which in flowering plants produce gametes. The male gametophytes, which produce sperm, " +
+      "are enclosed within pollen grains produced in the anthers. The female gametophytes are " +
+      "contained within the ovules produced in the carpels."
+  });
 
 
-    //todo: ----------------------------------------------------------------------------------------------
-    const showFlowers = () => {
-       return flowers.map((flower) => {
-            return (
-                <div className={"pot"}>
-                    <Flower
-                        image ={flower.image}
-                        commonName ={flower.commonName}
-                        scientificName = {flower.scientificName}
-                        info = {flower.info}
-                    />
-                </div>
-            );
-
-        })
-    }
-
-    const toSearch = () =>{
-        navigate('/search');
-    }
-
-    const handleSelectChange = (e) => {
-        setSelectedOption(e.target.value);
-    };
-
-    return (
-        <div className={"dashboard"}>
-            <Navbar/>
-            <div className={"main-panel-dashboard"}>
-
-                <div className={"inner-dashboard-panel"}>
-
-                    <div className={"my-plant"}>
-
-                        <div className={"dashboard-buttons"}>
-                            <button>Water all plants</button>
-                            <button>Expand all</button>
-                            <button>Collapse all</button>
-                            <select id="dropdown" value={selectedOption} onChange={handleSelectChange}>
-                                <option value="default">All plants</option>
-                                <option value="Watered">Watered</option>
-                                <option value="NotWatered">Not watered</option>
-                            </select>
-                        </div>
-                        <div className={"my-plant-panel"}>
-                            {flowers.length === 0 ?
-                                <div className={"pot"}>
-                                    <button style={{marginLeft: '35%'}} onClick={toSearch}>press me</button>
-                                </div>
-                                :
-                                showFlowers()
-                            }
-                        </div>
-                    </div>
-
-                    <div className={"side-panel"}>
-                        <h2>Notifications</h2>
-                        <div className={"notification-panel"}>
-                            <p>You need to water plants!</p>
-                            <p>I'm not joking</p>
-                            <p>Man, fr</p>
-                            <p>Man, please</p>
-                            <p>Man, they will die</p>
-                            <p>Mf, do it now!</p>
-                            <p>Last chance!</p>
-                            <p>I call the police</p>
-                            <p>They coming</p>
-                            <p>I farted under your pillow</p>
-                            <p>I ate taco as well</p>
-
-                        </div>
-                        <h2>Did you know that?</h2>
-                        <div className={"fun-fact-panel"}>
-                            <p>Lorem ipsum dolor sit amet,
-                                consectetur adipiscing elit.
-                                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                aliquip ex ea commodo consequat.
-                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                                sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </p>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+  //todo: ----------------------------------------------------------------------------------------------
+  const showFlowers = () => {
+    return flowers.map((flower) => {
+      return (
+        <div className={"pot"}>
+          <Flower
+            image={flower.image}
+            commonName={flower.commonName}
+            scientificName={flower.scientificName}
+            info={flower.info}
+          />
         </div>
-    );
+      );
+
+    })
+  }
+
+  const toSearch = () => {
+    navigate('/search');
+  }
+
+  const handleSelectChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
+
+  return (
+    <div className={"dashboard"}>
+      <Navbar/>
+      <div className={"main-panel-dashboard"}>
+
+        <div className={"inner-dashboard-panel"}>
+
+          <div className={"my-plant"}>
+
+            <div className={"dashboard-buttons"}>
+              <button>Water all plants</button>
+              <button>Expand all</button>
+              <button>Collapse all</button>
+              <select id="dropdown" value={selectedOption} onChange={handleSelectChange}>
+                <option value="default">All plants</option>
+                <option value="Watered">Watered</option>
+                <option value="NotWatered">Not watered</option>
+              </select>
+            </div>
+            <div className={"my-plant-panel"}>
+              {flowers.length === 0 ?
+                <div className={"pot"}>
+                  <button style={{marginLeft: '35%'}} onClick={toSearch}>press me</button>
+                </div>
+                :
+                showFlowers()
+              }
+            </div>
+          </div>
+
+          <div className={"side-panel"}>
+            <h2>Notifications</h2>
+            <div className={"notification-panel"}>
+              <p>You need to water plants!</p>
+              <p>I'm not joking</p>
+              <p>Man, fr</p>
+              <p>Man, please</p>
+              <p>Man, they will die</p>
+              <p>Mf, do it now!</p>
+              <p>Last chance!</p>
+              <p>I call the police</p>
+              <p>They coming</p>
+              <p>I farted under your pillow</p>
+              <p>I ate taco as well</p>
+
+            </div>
+            <h2>Did you know that?</h2>
+            <div className={"fun-fact-panel"}>
+              <p>Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit.
+                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Dashboard;
--- systemutv-2-frontend/src/Pages/Search/Search.js
+++ systemutv-2-frontend/src/Pages/Search/Search.js
@@ -1,34 +1,72 @@
 import './Search.css';
 import Navbar from "../../Navbar";
 import {useState} from "react";
+import Flower from "../Dashboard/Flower";
 
 const Search = () => {
 
-    const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState('');
+  const [plant, setPlant] = useState('');
+  const [plants, setPlants] = useState([]);
 
-    const handleSelectChange = (e) => {
-        setFilter(e.target.value);
-    }
+  const handleValue = (e) => {
+    setPlant(e.target.value);
+  }
 
-    return (
-        <div className={"search"}>
-            <Navbar/>
-            <div className={"main-panel-search"}>
-                <div className={"search-buttons"}>
-                    <input placeholder={"Search text here"}/>
-                    <button>Search</button>
-                    <select id="dropdown" value={filter} onChange={handleSelectChange}>
-                        <option value="common">Common name</option>
-                        <option value="scientific">Scientific name</option>
-                    </select>
-                </div>
+  const getPlants = () => {
+    // create a fetch request based on the value and filter
+    console.log(plant);
+    fetch(`http://localhost:7002/v1/plants/search?plant=${plant}`,
+    ).then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Request failed');
+    }).then(data => {
+      console.log(data);
+      setPlants(data);
+      plants.push(data);
+    });
 
-                <div className={"search-result-panel"}>
-                    <h2>I am flower</h2>
-                </div>
+  };
 
-            </div>
+  const showFlowers = () => {
+    return plants.map((plant) => {
+      return (
+        <div className={"pot"}>
+          <Flower
+            image={plant.image}
+            commonName={plant.commonName}
+            scientificName={plant.scientificName}
+            info={plant.info}
+          />
         </div>
-    );
+      );
+
+    })
+  }
+  const handleSelectChange = (e) => {
+    setFilter(e.target.value);
+  }
+
+  return (
+    <div className={"search"}>
+      <Navbar/>
+      <div className={"main-panel-search"}>
+        <div className={"search-buttons"}>
+          <input onChange={handleValue} placeholder={"Search text here"}/>
+          <button onClick={getPlants}>Search</button>
+          <select id="dropdown" value={filter} onChange={handleSelectChange}>
+            <option value="common">Common name</option>
+            <option value="scientific">Scientific name</option>
+          </select>
+        </div>
+
+        <div className={"search-result-panel"}>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 export default Search;
