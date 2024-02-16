3538ced6c734ec52a02ebde2721aa8eb2cd0c062
Filip Bengtegård <bengtegardbook@gmail.com>
2024-02-16 13:44:23 +0100

    add username and confirm email to register form

--- systemutv-2-frontend/src/Pages/Login/Register.js
+++ systemutv-2-frontend/src/Pages/Login/Register.js
@@ -3,48 +3,79 @@ import {Link, useNavigate} from "react-router-dom";
 import {useState} from "react";
 
 const Register = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [passwordMatch, setPasswordMatch] = useState(false);
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordMatch, setPasswordMatch] = useState(false);
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handleConfirmPasswordChange = (e) => {
-        setConfirmPassword(e.target.value);
-        checkPasswordMatch(password, e.target.value);
-    };
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+    checkPasswordMatch(password, e.target.value);
+  };
 
-    const checkPasswordMatch = (password, confirmPassword) => {
-        if (password === confirmPassword) {
-            setPasswordMatch(true);
-        } else {
-            setPasswordMatch(false);
-        }
-    };
-
-    const Submit = () => {
-       navigate("/dashboard");
+  const checkPasswordMatch = (password, confirmPassword) => {
+    if (password === confirmPassword) {
+      setPasswordMatch(true);
+    } else {
+      setPasswordMatch(false);
     }
+  };
 
-    return (
-        <div className={"login"}>
-            <div className="login-container">
-                <h1>Create your garden</h1>
-                <div className="form-container">
-                    <form action="#" method="post" className={"inputs-container"}>
-                        <input type="email" id="email" name="email" placeholder="Enter your email" required/>
-                        <input type="password" id="password" name="password" placeholder="Enter your password" onChange={handlePasswordChange} required/>
-                        <input type="password" id="confirm" name="password" placeholder="Confirm your password" onChange={handleConfirmPasswordChange} style={{ borderColor: passwordMatch ? "#386641" : "#bc4749" }} required/>
-                        <button onClick={Submit} disabled={!passwordMatch}>Register</button>
-                    </form>
-                </div>
-                <h5>Already have an account? <Link to={"/"}>Login here</Link> </h5>
-            </div>
+  const [email, setEmail] = useState('');
+  const [confirmEmail, setConfirmEmail] = useState('');
+  const [emailMatch, setEmailMatch] = useState(false);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handleConfirmEmailChange = (e) => {
+    setConfirmEmail(e.target.value);
+    checkEmailMatch(email, e.target.value);
+  }
+
+  const checkEmailMatch = (email, confirmEmail) => {
+    if (email === confirmEmail) {
+      setEmailMatch(true);
+    } else {
+      setEmailMatch(false);
+    }
+  }
+
+  const Submit = () => {
+    navigate("/dashboard");
+  }
+
+  return (
+    <div className={"login"}>
+      <div className="login-container">
+        <h1>Create your garden</h1>
+        <div className="form-container">
+          <form action="#" method="post" className={"inputs-container"}>
+            <input type="email" id="email" name="email" placeholder="Enter your email"
+                   onChange={handleEmailChange} required/>
+            <input type="email" id="email" name="email" placeholder="Confirm your email"
+                   onChange={handleConfirmEmailChange} style={{borderColor: emailMatch ? "#386641" : "#bc4749"}}
+                   required/>
+
+            <input type="username" id="username" name="username" placeholder="Enter your username" required/>
+
+            <input type="password" id="password" name="password" placeholder="Enter your password"
+                   onChange={handlePasswordChange} required/>
+            <input type="password" id="confirm" name="password" placeholder="Confirm your password"
+                   onChange={handleConfirmPasswordChange} style={{borderColor: passwordMatch ? "#386641" : "#bc4749"}}
+                   required/>
+            <button onClick={Submit} disabled={!passwordMatch}>Register</button>
+          </form>
         </div>
-    );
+        <h5>Already have an account? <Link to={"/"}>Login here</Link></h5>
+      </div>
+    </div>
+  );
 }
 export default Register;
