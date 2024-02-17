03ff726806d62d680fc3870cf65bec39638eef7f
AhmadToron <ahmadtoron@hotmail.com>
2024-02-17 19:54:07 +0100

    Register & Search
    implement register fetch to register a user.
    implement flower search fetch a json from our javalin backend

--- systemutv-2-frontend/src/Pages/Login/Register.js
+++ systemutv-2-frontend/src/Pages/Login/Register.js
@@ -5,9 +5,19 @@ import {useState} from "react";
 const Register = () => {
     const navigate = useNavigate();
 
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [passwordMatch, setPasswordMatch] = useState(false);
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
 
     const handlePasswordChange = (e) => {
         setPassword(e.target.value);
@@ -26,8 +36,34 @@ const Register = () => {
         }
     };
 
-    const Submit = () => {
-       navigate("/dashboard");
+    const Submit = async (event) => {
+        event.preventDefault();
+        if (!passwordMatch) {
+            alert("Passwords do not match!");
+            return;
+        }
+
+        try {
+            const response = await fetch('http://localhost:7002/v1/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                    username: username,
+                }),
+            });
+
+            if (response.status === 201) {
+                navigate("/dashboard");
+            } else {
+                alert("An error occurred" + response.statusText);
+            }
+        } catch (error) {
+            alert("Network error: " + error.message);
+        }
     }
 
     return (
@@ -35,14 +71,20 @@ const Register = () => {
             <div className="login-container">
                 <h1>Create your garden</h1>
                 <div className="form-container">
-                    <form action="#" method="post" className={"inputs-container"}>
-                        <input type="email" id="email" name="email" placeholder="Enter your email" required/>
-                        <input type="password" id="password" name="password" placeholder="Enter your password" onChange={handlePasswordChange} required/>
-                        <input type="password" id="confirm" name="password" placeholder="Confirm your password" onChange={handleConfirmPasswordChange} style={{ borderColor: passwordMatch ? "#386641" : "#bc4749" }} required/>
-                        <button onClick={Submit} disabled={!passwordMatch}>Register</button>
+                    <form onSubmit={Submit} className={"inputs-container"}>
+                        <input type="text" id="username" name="username" placeholder="Enter your username"
+                               onChange={handleUsernameChange} required/>
+                        <input type="email" id="email" name="email" placeholder="Enter your email"
+                               onChange={handleEmailChange} required/>
+                        <input type="password" id="password" name="password" placeholder="Enter your password"
+                               onChange={handlePasswordChange} required/>
+                        <input type="password" id="confirm" name="confirmPassword" placeholder="Confirm your password"
+                               onChange={handleConfirmPasswordChange}
+                               style={{borderColor: passwordMatch ? "#386641" : "#bc4749"}} required/>
+                        <button type="submit" disabled={!passwordMatch}>Register</button>
                     </form>
                 </div>
-                <h5>Already have an account? <Link to={"/"}>Login here</Link> </h5>
+                <h5>Already have an account? <Link to={"/"}>Login here</Link></h5>
             </div>
         </div>
     );
--- systemutv-2-frontend/src/Pages/Search/Search.js
+++ systemutv-2-frontend/src/Pages/Search/Search.js
@@ -1,34 +1,90 @@
 import './Search.css';
+import Flower from "../Dashboard/Flower";
 import Navbar from "../../Navbar";
 import {useState} from "react";
 
+
 const Search = () => {
 
-    const [filter, setFilter] = useState('');
+    const [filter, setFilter] = useState('common');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [flowers, setFlowers] = useState([]);
 
     const handleSelectChange = (e) => {
         setFilter(e.target.value);
     }
 
+    const handleInputChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const fetchFlowers = async () => {
+        try {
+            const response = await fetch(`http://localhost:7002/v1/plants/search?plant=${encodeURIComponent(searchTerm)}`);
+            if (!response.ok) {
+                throw new Error('Failed to fetch flowers');
+            }
+            const data = await response.json();
+            setFlowers(data.data);
+        } catch (error) {
+            console.error("Error fetching flowers:", error);
+            setFlowers([]);
+        }
+    };
+
+    // Render flowers in the search result panel
+    const renderFlowers = () => {
+        return flowers.map((flower, index) => (
+            <Flower
+                key={index}
+                image={flower.image_url}
+                commonName={flower.common_name}
+                scientificName={flower.scientific_name}
+                info={flower.synonyms}
+            />
+        ));
+    };
+
+
     return (
-        <div className={"search"}>
-            <Navbar/>
-            <div className={"main-panel-search"}>
-                <div className={"search-buttons"}>
-                    <input placeholder={"Search text here"}/>
-                    <button>Search</button>
+        <div className="search">
+            <Navbar />
+            <div className="main-panel-search">
+                <div className="search-buttons">
+                    <input
+                        placeholder="Search text here"
+                        value={searchTerm}
+                        onChange={handleInputChange}
+                    />
+                    <button onClick={fetchFlowers}>Search</button>
                     <select id="dropdown" value={filter} onChange={handleSelectChange}>
                         <option value="common">Common name</option>
                         <option value="scientific">Scientific name</option>
                     </select>
                 </div>
 
-                <div className={"search-result-panel"}>
-                    <h2>I am flower</h2>
+                <div className="search-result-panel">
+                    {flowers.length > 0 ? renderFlowers() : <p>No results to display</p>}
                 </div>
 
+                <div className="search-result-panel">
+                    {flowers.length > 0 ? (
+                        flowers.map((flower, index) => (
+                            <div key={index} className="flower-result">
+                                <img src={flower.imageURL} alt={flower.commonName}
+                                     style={{width: "100px", height: "100px"}}/>
+                                <h3>{flower.commonName}</h3>
+                                <p>{flower.scientificName}</p>
+                            </div>
+                        ))
+                    ) : (
+                        <p>No results to display</p>
+                    )}
+                </div>
             </div>
         </div>
     );
+
+
 }
 export default Search;
