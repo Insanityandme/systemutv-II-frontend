d3b25b63855dc1f54f578613f5a4b8a749a8e90b
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2024-02-27 18:25:14 +0100

    notification branch

---
+++ systemutv-2-frontend/src/Notification.js
@@ -0,0 +1,16 @@
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Login/Register";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Search from "./Pages/Search/Search";
+import Settings from "./Pages/Settings/Settings";
+
+const Notification = (props) => {
+
+    return (
+        <div>
+            <h3>Notification demo</h3>
+        </div>
+    );
+};
+
+export default Notification;
