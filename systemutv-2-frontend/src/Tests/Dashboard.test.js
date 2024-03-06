c480e3d9a208aace678537ee70c81a5c10756bb5
AhmadToron <ahmadtoron@hotmail.com>
2024-03-06 21:25:14 +0100

    Update Dashboard.test
    Update Dashboard.test

--- systemutv-2-frontend/src/Tests/Dashboard.test.js
+++ systemutv-2-frontend/src/Tests/Dashboard.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent} from '@testing-library/react';
+import { render, screen, fireEvent, waitFor} from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Dashboard from '../Pages/Dashboard/Dashboard';
 import '@testing-library/jest-dom';
@@ -7,21 +7,47 @@ import fetchMock from 'jest-fetch-mock';
 
 fetchMock.enableMocks();
 
-beforeEach(() => {
-    fetchMock.resetMocks();
-});
+describe('Dashboard Component', () => {
+    beforeEach(() => {
+        fetchMock.resetMocks();
+        sessionStorage.clear();
+        sessionStorage.setItem('userId', 'testUserId');
+    });
 
-// Test for initial render and fetching plants
-test('renders Dashboard and fetches plants', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify({ data: [] }));
+    it('fetches plants and displays their nicknames on the dashboard', async () => {
+        // Mock responses for both plants and a fun fact
+        const mockPlantsResponse = [
+            {
+                "id": 3,
+                "commonName": null,
+                "scientificName": null,
+                "family": null,
+                "imageURL": "string",
+                "nickname": "HGJGFJG",
+                "lastWatered": "1999-01-02",
+                "waterFrequency": -1,
+                "genus": null,
+                "light": 0
+            },
+        ];
 
-    render(
-        <BrowserRouter>
-            <Dashboard />
-        </BrowserRouter>
-    );
-    expect(screen.getByText(/Water all plants/i)).toBeInTheDocument();
+        const mockFunFactResponse = [{fact:"Plants can communicate."},];
 
+        fetchMock.mockResponses(
+            [JSON.stringify(mockPlantsResponse), { status: 200 }],
+            [JSON.stringify(mockFunFactResponse), { status: 200 }]
+        );
+
+        render(
+            <BrowserRouter>
+                <Dashboard />
+            </BrowserRouter>
+        );
+
+        await waitFor(() => {
+            expect(screen.getByText('HGJGFJG')).toBeInTheDocument();
+        });
+    });
 });
 
 // Test for navigating to the search page
